cad6fafdaad00bae7196b3f6c9d736d7b720af2c
k19 <kuhu19@users.noreply.github.com>
2016-08-25 09:39:37 +0530

    Update ForLoopExample.java

--- basicexamples/ForLoopExample.java
+++ basicexamples/ForLoopExample.java
@@ -2,7 +2,13 @@ package examples;
 
 public class ForLoopExample {
 	    public static void main(String args[]){
+                 //incremental for loop
 	         for(int i=10; i>1; i--){
+	              System.out.println("The value of i is: "+i);
+	         }
+                 
+                 //decremental for loop
+	         for(int i=0; i<10; i++){
 	              System.out.println("The value of i is: "+i);
 	         }
 	    }
