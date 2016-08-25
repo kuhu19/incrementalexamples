604c4fcba722cf9edab8d8ea3a22d467951de430
k19 <kuhu19@users.noreply.github.com>
2016-08-25 09:16:32 +0530

    Update HelloWorld.java

--- basicexamples/HelloWorld.java
+++ basicexamples/HelloWorld.java
@@ -1,17 +1,10 @@
 package examples;
 
-//this is a single line comment
-
-/*
- * this is multi line comment
- */
 /** Write a Java program that prints "Welcome to Java" on the console. */
 public class HelloWorld {
-	
   	public static void main (String[] args)
  	{
   		System.out.println("Welcome to Java");
- 
 	}		
 }
 
