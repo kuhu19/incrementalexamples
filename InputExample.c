e77ed59756b1ac7277ee96ba2ccf83cfb12fdf9e
k19 <kuhu19@users.noreply.github.com>
2016-08-26 11:56:01 +0530

    Update InputExample.c

--- InputExample.c
+++ InputExample.c
@@ -1,11 +1,13 @@
 /* User input example */
 
 #include <stdio.h>
+#include <conio.h>
 
 int main()
 {
     int this_is_a_number;
-
+    
+    clrscr();
     printf( "Please enter a number: " ); 
     scanf( "%d",&this_is_a_number); 
     printf( "You entered %d", this_is_a_number );
